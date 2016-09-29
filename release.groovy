47d0d16dfdf765cea979c528c188b9b786b848d9
rawlingsj <rawlingsj80@gmail.com>
2016-09-29 13:14:25 +0100

    fix f-m-p until we generate helm charts in a phase that gets .asc files created

--- release.groovy
+++ release.groovy
@@ -4,7 +4,7 @@ def updateDependencies(source){
   def properties = []
   // properties << ['<fabric8.console.version>','io/fabric8/console']
   properties << ['<fabric8.version>','io/fabric8/kubernetes-api']
-  properties << ['<fabric8.maven.plugin.version>','io/fabric8/fabric8-maven-plugin']
+  // properties << ['<fabric8.maven.plugin.version>','io/fabric8/fabric8-maven-plugin']
   properties << ['<fabric8.devops.version>','io/fabric8/devops/apps/jenkins']
   properties << ['<fabric8.forge.version>','io/fabric8/forge/apps/fabric8-forge']
 
